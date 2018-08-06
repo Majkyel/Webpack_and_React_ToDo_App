69c7532165c01ed996cfe814043d62b922ee8cf7
Michał Kuliś <michalkulis1993@gmail.com>
2018-08-06 13:49:15 +0200

    Update Title.js file

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,9 @@
 import react from 'react';
 
-const title = props => <div><h1>{props.title}</h1><h3>Task numbers: {props.length}</h3></div>;
+const title = props => 
+    <div>
+        <h1>{props.title}</h1>
+        <h3>Task numbers: {props.length}</h3>
+    </div>;
 
 export default title;
